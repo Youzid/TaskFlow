4763c28ecded5a58488fb259db070cdbdd979cf8
youcefbenbouzid <byoucef@sefrone.com>
2024-06-25 17:39:30 +0200

    create board popover

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -6,5 +6,11 @@ export const CreateBoardSchema = z.object({
       required_error: "Title is required",
       invalid_type_error: "Title is required",
     })
-    .min(3, "Title is short"),
+    .min(3, {
+      message: "Title is too short.",
+    }),
+  image: z.string({
+    required_error: "Image is required",
+    invalid_type_error: "Image is required",
+  }),
 });
